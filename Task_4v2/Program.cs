eb90e583a747ee6745182778f465a40fb3e1895e
AVTahion <avkontakt40@gmail.com>
2019-03-05 18:09:25 +0300

    Task_4v2

---
+++ Task_4v2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  4.  Первая команда увеличивает число на экране на 1, вторая увеличивает его в 2 раза. Определить, сколько существует программ, которые преобразуют число 3 в число 20:
+          а. С использованием массива.
+          b. *С использованием рекурсии.
+
+    Александр Кушмилов
+*/
+
+namespace Task_4v2
+{
+    class Program
+    {
+        static int NumberOfSolutions(int start, int end, int p, int m)
+        {
+            int[] arr = new int[end + 1];
+            arr[start] = 1;
+            for (int i = start + 1; i <= end; i++)
+            {
+                arr[i] = i % m == 0 ? arr[i - p] + arr[i / m] : arr[i - p];
+            }
+            return arr[end];
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfSolutions(3, 20, 1, 2));
+            Console.ReadKey();
+        }
+    }
+}
